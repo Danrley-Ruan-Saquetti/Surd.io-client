7bc8f4120ac483209bcf4467eb74fbc8769885cb
Dan Ruan <danrsaquetti@gmail.com>
2022-12-20 17:20:35 -0300

    create connection with server

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import socket from './service/socket'
 import "./styles/GlobalStyles.css"
 
+socket.on("connect", () => {
+    socket.on("hello", msg => { console.log(msg); console.log("") })
+})
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
 )
